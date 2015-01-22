8b7cf67e36b970b4fa70f10b17da4d540aed5eda
Lisa Matter <lisamatter@Lisas-MacBook-Pro.local>
2015-01-21 21:48:05 -0700

    Initial Commit

---
+++ Course Exercises/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Course Exercises
+//
+//  Created by Lisa Matter on 1/21/15.
+//  Copyright (c) 2015 MSSE 650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Course Exercises/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Course Exercises
+//
+//  Created by Lisa Matter on 1/21/15.
+//  Copyright (c) 2015 MSSE 650. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
